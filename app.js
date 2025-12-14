6da217ccf010ff2dd968451be0c53cd90a2a5fe6
Ryju00 <133570676+Ryju00@users.noreply.github.com>
2025-12-14 17:26:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": { "enable": true, "value_area": 800 }
+    },
+    "color": { "value": "#ffffff" },
+    "opacity": { "value": 0.7, "random": false },
+    "size": { "value": 4, "random": true },
+    "line_linked": { "enable": false },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "bottom",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": false },
+      "onclick": { "enable": false }
+    }
+  },
+  "retina_detect": true
+});
